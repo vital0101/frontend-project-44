682317924ae5dcf2ecd637d05ca138c84d18342b
Vitaliy Vaskin <vital0101@yandex.ru>
2023-02-28 17:22:59 +0300

    refactoring file

--- src/games/brainEven.js
+++ src/games/brainEven.js
@@ -1,48 +1,14 @@
-#!/usr/bin/env node
+import { runGame, getRandomInteger } from '../index.js';
 
-import readlineSync from 'readline-sync';
+// Функция - возвращает массив из выражения и результат вычесленного выражения
+const getArr = () => {
+  const randomExpression = getRandomInteger(1, 10);
+  const correctAnswer = randomExpression % 2 === 0 ? 'yes' : 'no';
+  return [randomExpression, correctAnswer];
+}
 
-const arrNumbers = [15, 7, 6];
-
-console.log('Welcome to the Brain Games!');
-
-const userName = readlineSync.question('May I have your name? ');
-
-console.log(`Hello, ${userName}!`);
-
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-// const isRightAnswer = () => {
-//   console.log(`Your answer: ${response}`);
-//   console.log('Correct!');
-// };
-
-const isWrongAnswer = () => {
-  console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.`);
-  console.log(`Let's try again, ${userName}!`)
+export const runEven = () => {
+  const nameGame = 'brain-even';
+  runGame(nameGame, getArr());
+  return;
 };
-
-const isNotCorrectAnswer = () => {
-  console.log(`Answer 'yes' if the number is even, otherwise answer 'no'.`);
-  console.log(`Let's try again, ${userName}!`);
-};
-
-const getResult = () => {
-  for (let num of arrNumbers) {
-    const response = readlineSync.question(`Question: ${num} / `);
-    if ((response === 'no' && num % 2 !== 0) || (response === 'yes' && num % 2 === 0)) {
-      console.log(`Your answer: ${response}`);
-    } else if ((response === 'yes' && num % 2 !== 0) || (response === 'no' && num % 2 === 0)) {
-      isWrongAnswer();
-      return;
-    } else {
-      isNotCorrectAnswer();
-      return;
-    }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
-
-getResult();
-
