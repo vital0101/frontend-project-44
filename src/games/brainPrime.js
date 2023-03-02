843807c07588eb6ef75191f1331df1c430c5a253
Vitaliy Vaskin <vital0101@yandex.ru>
2023-03-02 13:12:44 +0300

    add file brainPrime

---
+++ src/games/brainPrime.js
@@ -0,0 +1,23 @@
+import { runGame, getRandomInteger } from '../index.js';
+
+// Функция - возвращает массив состоящий из выражения и результата вычесленного выражения
+const getRandomExpression = () => {
+  let prime = true;
+  let randomExpression = getRandomInteger(2, 50);
+  for (let i = 2; i < randomExpression - 1; i += 1) {
+    if (randomExpression % i === 0) {
+      prime = false;
+  }
+}
+  const correctAnswer = prime ? 'yes' : 'no';
+  return [randomExpression, correctAnswer];
+};
+
+// Функция запуска игры
+const runPrime = () => {
+  const nameGame = 'brain-prime';
+  runGame(nameGame, getRandomExpression);
+  return;
+}
+
+export { runPrime };
