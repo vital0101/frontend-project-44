bbf6e8cb3689b9217d2f03658f6bf894cfd7489a
Vitaliy Vaskin <vital0101@yandex.ru>
2023-03-06 18:09:32 +0300

    refacoring file

--- src/games/brainCalc.js
+++ src/games/brainCalc.js
@@ -6,29 +6,49 @@ import { getRandomInteger, runGame } from '../index.js';
 // Функция - получает случайный арифметический оператор в виде строки
 const getRandomOperator = () => {
   const arr = ['-', '+', '*'];
-  const randomArrEl = arr[Math.floor(Math.random() * arr.length)];
+  const randomArrEl = arr[getRandomInteger(0, 2)];
   return randomArrEl;
+};
+
+// Функция - возвращает случайные операнды,оператор и разультат выражения
+const getResultExpression = () => {
+  const randomOperator = getRandomOperator();
+  const randomNumOne = getRandomInteger(1, 10);
+  const randomNumTwo = getRandomInteger(1, 10);
+  let resultExpression;
+  switch (randomOperator) {
+    case '-':
+      resultExpression = randomNumOne - randomNumTwo;
+      break;
+    case '+':
+      resultExpression = randomNumOne + randomNumTwo;
+      break;
+    case '*':
+      resultExpression = randomNumOne * randomNumTwo;
+      break;
+    default:
+      resultExpression = 0;
+  }
+  return [randomNumOne, randomOperator, randomNumTwo, resultExpression];
 };
 
 // Функция - возвращает массив состоящий из выражения и результата вычесленного выражения
 const getRandomExpression = () => {
-  const randomOperator = getRandomOperator();
-  const randomNumOne = getRandomInteger(1, 10);
-  const randomNumTwo = getRandomInteger(1, 10);
+  const [randomNumOne, randomOperator, randomNumTwo, resultExpression] = getResultExpression();
   let expression;
   let correctAnswer;
   switch (randomOperator) {
     case '-':
       expression = `${randomNumOne} - ${randomNumTwo}`;
-      correctAnswer = randomNumOne - randomNumTwo;
+      correctAnswer = resultExpression;
       return [expression, correctAnswer];
     case '+':
       expression = `${randomNumOne} + ${randomNumTwo}`;
-      correctAnswer = randomNumOne + randomNumTwo;
+      correctAnswer = resultExpression;
       return [expression, correctAnswer];
     case '*':
       expression = `${randomNumOne} * ${randomNumTwo}`;
-      correctAnswer = randomNumOne * randomNumTwo;
+      correctAnswer = resultExpression;
       return [expression, correctAnswer];
     default:
       return 'This is an empty string';
@@ -40,8 +60,8 @@ const getArr = () => getRandomExpression();
 
 // Функция запуска игры
 const runCalc = () => {
-  const nameGame = 'brain-calc';
-  runGame(nameGame, getArr);
+  const gameCondition = 'What is the result of the expression?';
+  runGame(gameCondition, getArr);
 };
 
 export default runCalc;
