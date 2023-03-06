52e7970c2586088e45e689d0f20258845459ae93
Vitaliy Vaskin <vital0101@yandex.ru>
2023-03-06 18:12:56 +0300

    refactoring file

--- src/games/brainProgression.js
+++ src/games/brainProgression.js
@@ -1,44 +1,33 @@
 import { runGame, getRandomInteger } from '../index.js';
 
-// Функция - возвращает массив в виде двух чисел (начало арифметической прогрессии)
-const getStartProgression = () => {
-  const startProgressionNum = [];
-  const randomNumOne = getRandomInteger(1, 20);
-  const randomNumTwo = getRandomInteger(1, 20);
-  if (randomNumOne >= randomNumTwo) {
-    startProgressionNum.push(randomNumTwo, randomNumOne);
-  } else {
-    startProgressionNum.push(randomNumOne, randomNumTwo);
-  }
-  return startProgressionNum;
-};
-
 // Функция - возвращает массив с арифметической прогрессией
 const getProgression = () => {
-  const count = getRandomInteger(5, 10);
-  const arr = getStartProgression();
-  for (let i = 0; i < count; i += 1) {
-    const progNum = (arr[i + 1] - arr[i]) + arr[i + 1];
-    arr.push(progNum);
+  const lengthProgression = 10;
+  const startProgression = getRandomInteger(1, 20);
+  const stepProgression = getRandomInteger(1, 20);
+  const array = [];
+  for (let i = 0; i <= lengthProgression; i += 1) {
+    const progNum = startProgression + i * stepProgression;
+    array.push(progNum);
   }
-  return arr;
+  return array;
 };
 
 // Функция - возвращает массив состоящий из выражения и результата вычесленного выражения
 const getRandomExpression = () => {
-  const newArr = getProgression();
-  const randomArrEl = newArr[Math.floor(Math.random() * newArr.length)];
-  const correctAnswer = randomArrEl;
-  const indexEl = newArr.indexOf(randomArrEl);
-  newArr[indexEl] = '..';
-  const strNewArr = newArr.join(' ');
-  return [strNewArr, correctAnswer];
+  const array = getProgression();
+  const randomarrayEl = array[Math.floor(Math.random() * array.length)];
+  const correctAnswer = randomarrayEl;
+  const indexEl = array.indexOf(randomarrayEl);
+  array[indexEl] = '..';
+  const strFromArray = array.join(' ');
+  return [strFromArray, correctAnswer];
 };
 
 // Функция запуска игры
 const runProgression = () => {
-  const nameGame = 'brain-progression';
-  runGame(nameGame, getRandomExpression);
+  const gameCondition = 'What number is missing in the progression?';
+  runGame(gameCondition, getRandomExpression);
 };
 
 export default runProgression;
