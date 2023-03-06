dfbaa9709389fb204fddb5c29584aabe19be0fbc
Vitaliy Vaskin <vital0101@yandex.ru>
2023-03-06 18:12:35 +0300

    refactoring file

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -3,20 +3,20 @@ import { runGame, getRandomInteger } from '../index.js';
 // Функция - возвращает массив состоящий из выражения и результата вычесленного выражения
 const getRandomExpression = () => {
   let prime = true;
-  const randomExpression = getRandomInteger(2, 50);
-  for (let i = 2; i < randomExpression - 1; i += 1) {
-    if (randomExpression % i === 0) {
+  const randomNumber = getRandomInteger(2, 50);
+  for (let i = 2; i < Math.sqrt(randomNumber); i += 1) {
+    if (randomNumber % i === 0) {
       prime = false;
     }
   }
   const correctAnswer = prime ? 'yes' : 'no';
-  return [randomExpression, correctAnswer];
+  return [randomNumber, correctAnswer];
 };
 
 // Функция запуска игры
 const runPrime = () => {
-  const nameGame = 'brain-prime';
-  runGame(nameGame, getRandomExpression);
+  const gameCondition = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+  runGame(gameCondition, getRandomExpression);
 };
 
 export default runPrime;
